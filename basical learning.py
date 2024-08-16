50a43e8e91abbfb63e63e124df6a744d2c0f15d1
ZhouHongYi <2746486175@qq.com>
2024-08-16 19:22:03 +0800

    修改gitignore

--- basical learning.py
+++
@@ -1,6 +0,0 @@
-import xlsxwriter
-data=[['Name', 'Web', 'Content'],['Alice', 'https://example.com/alice', 'This is Alice\'s content.'],['Bob', 'https://example.com/bob', 'This is Bob\'s content.']]
-xlsx = xlsxwriter.Workbook('python.xlsx')
-sheet = xlsx .add_worksheet('Sheet1')
-sheet.write_row('A1', data[0])
-xlsx.close()
